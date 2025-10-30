9b7dbc8f836bc2b3cdead33aed5f30b2a27ae408
yotam-biu <nagaryo3@biu.ac.il>
2025-10-30 11:37:47 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,2 +1,7 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    if (num1 > num2) and (num1 > num3):
+       return num1 
+    elif (num2 > num3):
+       return num2
+    else:
+       return num3
